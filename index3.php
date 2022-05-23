76308e07c99b49ebfefd5e087fd726bf89180b38
magdaoliver <magdix33@hotmail.com>
2022-05-23 17:40:47 +0200

    funciones y ejercicios nuevos

---
+++ index3.php
@@ -0,0 +1,7 @@
+<?php 
+ $subtitular = "Subtitular 1 del ejecicio Require";
+ echo "<h1>Index 3 del ejercicio </h1>";
+ require 'vista3.php';
+ $subtitular = "Subtitular 2";
+ require 'vista3.php';
+ ?>
