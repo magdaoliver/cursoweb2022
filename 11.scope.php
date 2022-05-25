620817b9ee1f9f9eeb2bbb295c9f4c99dd9e84d2
magdaoliver <105653781+magdaoliver@users.noreply.github.com>
2022-05-25 09:27:23 +0200

    Update 11.scope.php

--- 11.scope.php
+++ 11.scope.php
@@ -19,25 +19,25 @@ echo $numero; //Muestro el 7
 // echo $numero; //Fuera sigue valiendo el 7
 
 # Lo mismo si definimos en la función y queremos utilizarla fuera
- function mostrarNumero(){
- 	$numero = 10;
- }
- mostrarNumero();
- echo $numero; //No se puede acceder porque la variable esta dentro de la función, necesitaremos el "return"
+//  function mostrarNumero(){
+//  	$numero = 10;
+//  }
+//  mostrarNumero();
+//  echo $numero; //No se puede acceder porque la variable esta dentro de la función, necesitaremos el "return"
 
-function mostrarNumero(){
-	$numero = 10;
-	return $numero;
-}
+// function mostrarNumero(){
+// 	$numero = 10;
+// 	return $numero;
+// }
 
-function mostrarNumero(){
-	$numero = 10;
-	echo $numero;
-}
+// function mostrarNumero(){
+// 	$numero = 10;
+// 	echo $numero;
+// }
 
-mostrarNumero();//10
-$numero = mostrarNumero(); //Asigno $numero =10;
-echo $numero; //10
+// mostrarNumero();//10
+// $numero = mostrarNumero(); //Asigno $numero =10;
+// echo $numero; //10
 
 #RESUMEN:
 //1. Por parámetro para entrar la variable en la función
